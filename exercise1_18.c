8660db88ed3eacd4a511c4c3af933aa526eed33f
mdluo <1912148105@qq.com>
2019-09-04 09:54:01 +0800

    完成的习题1-18

--- exercise1_18.c
+++ exercise1_18.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
 #define maxlines 1000
-int 
+int getlines(char s[],int lim)
+{
+	int c,i,j;
+	for (i = 0;i < lim-1 && (c = getchar()) != EOF && c != '\n'; ++i )
+	{
+		s[i]=c;
+	}
+	if(c == '\n')
+	{
+	
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
 int main()
 {
+	int len,i;
+	char line[maxlines];
+	while((len=getlines(line,maxlines))>0)
+	{
+		i=len-2;
+        	for(i>=0;((line[i]==' ')||(line[i]=='\t'));--i)
+        	        ;
+        	if(i>=0)
+        	{
+                	line[i+1] = '\n';
+                	
+                	line[i+2] = '\0';
+                	printf("%s",line);
+        	}	
+	}
 	return 0;
 }
