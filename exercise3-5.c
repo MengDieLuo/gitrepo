56f6d552c8a17795b70addcfb218dc084d901ec6
mdluo <1912148105@qq.com>
2019-09-10 21:11:58 +0800

    习题3-5

---
+++ exercise3-5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+void reverse(char s[])
+{
+        int c,i,j;
+        for ( i = 0, j = strlen(s)-1; i < j; i++, j--)
+        {
+                c=s[i];
+                s[i]=s[j];
+                s[j]=c;
+        }
+}
+void itob(int n,char s[],int b)
+{
+        int i=0,j,sign=n;
+        if(sign<0)
+		n=-n;
+	do{
+                j=n%b;
+		s[i++]=(j<=9)?j+'0':j+'a'-10;
+        }while((n /= 10)>0);
+        if(sign < 0)
+        {
+                s[i++] = '-';
+        }
+        s[i] = '\0';
+        reverse(s);
+}
+int main()
+{
+	int n,base;
+	char s[100];
+	n=42425;
+	base=16;
+	itob(n,s,base);
+	printf("%s\n",s);
+
+	return 0;
+}
